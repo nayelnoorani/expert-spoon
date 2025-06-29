fd19d9367d8c7af9fac9e8b2274e8a220c400cdf
Nayel Noorani <nayel.noorani@gmail.com>
2025-06-28 22:07:23 -0500

    Building basic frontend

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+// Serve static files first
+app.use(express.static(path.join(__dirname, 'frontend')));
+
+// Optional fallback route if no file matches
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 });
 
 app.listen(PORT, () => {
